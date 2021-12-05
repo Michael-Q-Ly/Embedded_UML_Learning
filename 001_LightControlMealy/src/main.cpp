9a141a7b86d1c892978177fee3f8a6cbbf485772
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2021-12-05 01:00:04 -0600

    Start first instructor-led exercise: LED PWM Mealy Machine

---
+++ 001_LightControlMealy/src/main.cpp
@@ -0,0 +1,25 @@
+/*
+ *  Name: Michael Ly
+ *  Date: December 5, 2021
+ *  Description: Change LED brightness with PWM using a Mealy Machine.
+ *  Target: Arduino Uno R3
+ */
+
+/*
+ * enum in C/C++ is a user-defined data type
+ * Use it to define your own data type
+ * Provides clarity to your program
+ * To use enum, enumerator-list must be known in advance
+ * (a finite list set of named integer constant values a
+ * data type can take on)
+ */
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
