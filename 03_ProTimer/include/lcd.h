8e41b6e55815cba6322cbf9a27293085d30e78ec
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2021-12-07 00:53:21 -0600

    Fix errors in function definitions and parameters for LCD

--- 03_ProTimer/include/lcd.h
+++ 03_ProTimer/include/lcd.h
@@ -8,12 +8,12 @@ extern void lcd_scroll_left             ( void ) ;
 extern void lcd_scroll_right            ( void ) ;
 extern void lcd_set_cursor              ( int r, int c ) ;
 extern void lcd_no_auto_scroll          ( void ) ;
-extern void lcd_begin                   ( void ) ;
+extern void lcd_begin                   ( uint8_t cols, uint8_t rows ) ;
 extern void lcd_move_cursor_R_to_L      ( void ) ;
 extern void lcd_move_cursor_L_to_R      ( void ) ;
 extern void lcd_cursor_off              ( void ) ;
 extern void lcd_blinkoff                ( void ) ;
-extern void lcd_insert_number           ( int num ) ;
+extern void lcd_print_number            ( int num ) ;
 extern void lcd_print_string            ( String s ) ;
 
 #endif  /* LCD_H_ */
