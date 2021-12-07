35a26b699463d4bde0944a3f7d3781522a6027e2
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2021-12-07 00:27:58 -0600

    Add LCD function prototypes and add LiquidCrystal Library

--- 03_ProTimer/include/lcd.h
+++ 03_ProTimer/include/lcd.h
@@ -1,5 +1,19 @@
 #ifndef LCD_H_
 #define LCD_H_
+#include "main.h"
 
+extern void lcd_clear                   ( void ) ;
+extern void lcd_print_char              ( char c ) ;
+extern void lcd_scroll_left             ( void ) ;
+extern void lcd_scroll_right            ( void ) ;
+extern void lcd_set_cursor              ( int r, int c ) ;
+extern void lcd_no_auto_scroll          ( void ) ;
+extern void lcd_begin                   ( void ) ;
+extern void lcd_move_cursor_R_to_L      ( void ) ;
+extern void lcd_move_cursor_L_to_R      ( void ) ;
+extern void lcd_cursor_off              ( void ) ;
+extern void lcd_blinkoff                ( void ) ;
+extern void lcd_insert_number           ( int num ) ;
+extern void lcd_print_string            ( String s ) ;
 
 #endif  /* LCD_H_ */
